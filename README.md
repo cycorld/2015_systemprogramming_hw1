c0b8d79888b06d57c2604ec8148b4882d2c1d562
cycorld <choi@likelion.net>
2015-03-10 14:47:58 +0900

    .out ignore

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Run
 
 ```
-$ gcc copy_ij.c -o ij
-$ gcc copy_ji.c -o ji
+$ gcc copy_ij.c -o ij.out
+$ gcc copy_ji.c -o ji.out
 $ ruby run.rb
 ```
--- run.rb
+++ run.rb
@@ -1,25 +1,28 @@
 require 'csv'
 
+RESOLUTION = 2
+ITERATION = 30
+
 result_ij = Hash.new
 result_ji = Hash.new
 temp = Array.new
 
-1.upto 255 do |s|
-  size = s * 4
-  20.times do |try|
-    result = %x[./ij #{size}].to_f
-    STDOUT.write "\rIJ | (#{(size - 1) * 20 + try} / 20460) RESULT : #{result}"
+1.upto (1024 / RESOLUTION - 1) do |s|
+  size = s * RESOLUTION
+  ITERATION.times do |try|
+    result = %x[./ij.out #{size}].to_f
+    STDOUT.write "\rIJ | (#{(size - 1) * ITERATION + try} / #{1024 * ITERATION}) RESULT : #{result}"
     temp << result
   end
   result_ij["#{4 * size * size}"] = temp.inject{|sum, x| sum + x}.to_f / temp.size
   temp = Array.new
 end
 
-1.upto 255 do |s|
-  size = s * 4
-  20.times do |try|
-    result = %x[./ji #{size}].to_f
-    STDOUT.write "\rJI | (#{(size - 1) * 20 + try} / 20460) RESULT : #{result}"
+1.upto (1024 / RESOLUTION - 1) do |s|
+  size = s * RESOLUTION
+  ITERATION.times do |try|
+    result = %x[./ji.out #{size}].to_f
+    STDOUT.write "\rJI | (#{(size - 1) * ITERATION + try} / #{1024 * ITERATION}) RESULT : #{result}"
     temp << result
   end
   result_ji["#{4 * size * size}"] = temp.inject{|sum, x| sum + x}.to_f / temp.size
@@ -28,7 +31,7 @@ end
 
 puts result_ij
 puts result_ji
-CSV.open("result.csv", "w") do |csv|
+CSV.open("result_res#{RESOLUTION}_iter#{ITERATION}_#{Time.now.to_i}.csv", "w") do |csv|
   csv << ["type", "byte", "time"]
   result_ij.each do |k, v|
     csv << ["ij", k, v]
